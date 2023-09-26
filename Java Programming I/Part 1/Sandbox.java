193700681bb188da83ea4e82af199aa993d2a6ce
Mansoor-Zafar <145301137+Mansoor-Zafar@users.noreply.github.com>
2023-09-25 22:43:46 -0400

    Update Sandbox.java

--- Java Programming I/Part 1/Sandbox.java
+++ Java Programming I/Part 1/Sandbox.java
@@ -3,3 +3,7 @@ public class Sandbox {
         // Write your program here
     }
 }
+/*
+The line "// Write your program here" is a line comment, and the computer will ignore it when executing the program.
+First, submit the exercise template to be checked on the server without changing anything. In this way, you practice submitting an exercise.
+*/
